deb26b7b88df06a5fc1bac0446b89d4cad97a9c9
Lorenzo Moscati <lorenzo@moscati.page>
2024-02-25 21:12:28 +0100

    Remove function from first round

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ def main():
     ts = ts_file.get_data()
     print("\nTime series data:")
     print(ts)
-    print("\nPositive yearly deltas from range 1949-1960:")
-    print(compute_increments(ts, "1949", "1960"))
     print("\nUnit tests:")
     tests = unittest.TestLoader().loadTestsFromModule(test_esame)
     unittest.TextTestRunner(verbosity=2).run(tests)
--- test_esame.py
+++ test_esame.py
@@ -1,7 +1,6 @@
 import unittest
 from esame import ExamException
 from esame import CSVTimeseriesFile
-from esame import compute_increments
 
 class TestExamException(unittest.TestCase):
     def test_message(self):
@@ -37,82 +36,6 @@ class TestCSVTimeseriesFile(unittest.TestCase):
             ['1956-01', 8],
             ['1957-01', 9]
         ])
-
-class TestComputeIncrements(unittest.TestCase):
-    def test_valid_range(self):
-        data = [
-            ['2010-01', 1],
-            ['2011-01', 2],
-            ['2012-01', 3],
-            ['2013-01', 4],
-            ['2014-01', 5],
-            ['2015-01', 6],
-            ['2016-01', 7],
-            ['2017-01', 8],
-            ['2018-01', 9]
-        ]
-        first_year = '2011'
-        last_year = '2017'
-        expected_result = {
-            '2011-2012': 1.0,
-            '2012-2013': 1.0,
-            '2013-2014': 1.0,
-            '2014-2015': 1.0,
-            '2015-2016': 1.0,
-            '2016-2017': 1.0
-        }
-        self.assertEqual(compute_increments(data, first_year, last_year), expected_result)
-
-    def test_invalid_range(self):
-        data = [
-            ['2010-01', 1],
-            ['2011-01', 2],
-            ['2012-01', 3],
-            ['2013-01', 4],
-            ['2014-01', 5],
-            ['2015-01', 6],
-            ['2016-01', 7],
-            ['2017-01', 8],
-            ['2018-01', 9]
-        ]
-        first_year = '2019'
-        last_year = '2020'
-        with self.assertRaises(ExamException):
-            compute_increments(data, first_year, last_year)
-
-    def test_non_numeric_year(self):
-        data = [
-            ['2010-01', 1],
-            ['2011-01', 2],
-            ['2012-01', 3],
-            ['2013-01', 4],
-            ['2014-01', 5],
-            ['2015-01', 6],
-            ['2016-01', 7],
-            ['2017-01', 8],
-            ['2018-01', 9]
-        ]
-        first_year = '2011'
-        last_year = 'abc'
-        with self.assertRaises(ExamException):
-            compute_increments(data, first_year, last_year)
-
-    def test_first_year_greater_than_last_year(self):
-        data = [
-            ['2010-01', 1],
-            ['2011-01', 2],
-            ['2012-01', 3],
-            ['2013-01', 4],
-            ['2014-01', 5],
-            ['2015-01', 6],
-            ['2016-01', 7],
-            ['2017-01', 8],
-            ['2018-01', 9]
-        ]
-        first_year = '2018'
-        last_year = '2017'
-        with self.assertRaises(ExamException):
-            compute_increments(data, first_year, last_year)
 
 if __name__ == '__main__':
     unittest.main()
