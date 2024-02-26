e6d8e39a988d8a38b24fa7a0be020ceb259e0e92
Lorenzo Moscati <lorenzo@moscati.page>
2024-02-26 18:03:33 +0100

    Add tests

--- test_esame.py
+++ test_esame.py
@@ -1,6 +1,7 @@
 import unittest
 from esame import ExamException
 from esame import CSVTimeseriesFile
+from esame import find_min_max
 
 class TestExamException(unittest.TestCase):
     def test_message(self):
@@ -36,6 +37,27 @@ class TestCSVTimeseriesFile(unittest.TestCase):
             ['1956-01', 8],
             ['1957-01', 9]
         ])
+
+class TestFindMinMax(unittest.TestCase):
+    def test_single_month_year(self):
+        data = [['2022-01', 10]]
+        expected_result = {}
+        self.assertEqual(find_min_max(data), expected_result)
+
+    def test_multiple_months_year(self):
+        data = [['2022-01', 10], ['2022-02', 20], ['2022-03', 15]]
+        expected_result = {'2022': {'min': ['01'], 'max': ['02']}}
+        self.assertEqual(find_min_max(data), expected_result)
+
+    def test_multiple_months_year_same_value(self):
+        data = [['2022-01', 10], ['2022-02', 10], ['2022-03', 10]]
+        expected_result = {'2022': {'min': ['01', '02', '03'], 'max': ['01', '02', '03']}}
+        self.assertEqual(find_min_max(data), expected_result)
+
+    def test_multiple_years(self):
+        data = [['2022-01', 10], ['2022-02', 20], ['2023-01', 5], ['2023-02', 15]]
+        expected_result = {'2022': {'min': ['01'], 'max': ['02']}, '2023': {'min': ['01'], 'max': ['02']}}
+        self.assertEqual(find_min_max(data), expected_result)
 
 if __name__ == '__main__':
     unittest.main()
