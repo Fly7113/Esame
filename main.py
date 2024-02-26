397448daea4ae3ce2f32f1c41b9edaf0aa8ba7be
Lorenzo Moscati <lorenzo@moscati.page>
2024-02-26 18:05:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ import test_esame
 def main():
     ts_file = CSVTimeseriesFile(name='data.csv')
     ts = ts_file.get_data()
+    min_max = find_min_max(ts)
     print("\nTime series data:")
     print(ts)
+    print("\nMinimum and maximum values:")
+    print(min_max)
     print("\nUnit tests:")
     tests = unittest.TestLoader().loadTestsFromModule(test_esame)
     unittest.TextTestRunner(verbosity=2).run(tests)
